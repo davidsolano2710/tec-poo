ef66a9733fdbf9d24b0b328b8d40f511efd34b21
David Solano <davsolano@estudiantec.cr>
2024-03-15 00:56:49 -0600

    main merge Tarea-3

--- tareas/hello-world-net/README.md
+++ tareas/hello-world-net/README.md
@@ -2,3 +2,16 @@ Aplicacion minima de .Net
 =========================
 
 Responder las preguntas aqui 
+
+a. dotnet build para .Net y javac archivo.java
+
+b. dotnet clean limpia los recursos de la ultima compilacion y para java utilizamos el build.sh
+el cual se encarga de eliminar la carpeta bin
+
+c. dotnet run para correr programas .Net y para java está el java archivo solamente
+
+d. Program.cs
+
+e. 2
+
+f. dll, se crea cuando se corre el comando dotnet build
--- tareas/hello-world-net/obj/Debug/net8.0/HelloWorldNet.AssemblyInfo.cs
+++ tareas/hello-world-net/obj/Debug/net8.0/HelloWorldNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227884716c88b188bfeebeced3e1d930b470124e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8ee03bbf246dae2c4a4824dd51739bc46fa067")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
